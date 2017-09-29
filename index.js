53f130906dbe31375dacf03e8b3ba0c57bd818b3
skalum <stkalum@gmail.com>
2017-09-29 19:17:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var facts = new Array();
 
   for (var i = 0; i < musicians.length; i++) {
-    facts.push("${musicians[i]} plays ${instruments[i]}");
+    facts.push(`${musicians[i]} plays ${instruments[i]}`);
   }
 
   return facts;
